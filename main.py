a05689b16cf1e81d4c972234e3c874a1a0c6ed7b
Corentin DRAULT <corentin.drault@orange.fr>
2020-11-07 11:50:34 +0100

    update template use

--- main.py
+++ main.py
@@ -6,5 +6,12 @@ def index():
     if request.method == 'POST':
         print(request.form['phrase'])
         #Request the ML app
-        return redirect(url_for('index'))
-    return render_template('index.html')
+
+
+        #result is the value of the sentiment
+        result = "Lorem ipsum"
+        return redirect(url_for('index', result=result))
+    result = request.args.get('result')
+    if result is None:
+    	return render_template('index.html')
+    return render_template('index.html', result=result)
